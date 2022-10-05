825204429f19a75d6baff6a2fc7bd664da39825e
abu bokor suraj <abusurajbd@gmail.com>
2022-10-05 10:04:14 +0600

    remove item from cart

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addToDb } from '../../utilities/fakeDb';
+import { addToDb, removeFromeDb } from '../../utilities/fakeDb';
 const Cosmetic = (props) => {
     const {cosmetic} = props;
     const {id,name,price} = cosmetic;
@@ -7,12 +7,16 @@ const Cosmetic = (props) => {
     const addToCart = id =>{
         addToDb(id);
     }
+    const removeFromCart = id=>{
+        removeFromeDb(id);
+    }
     return (
         <div className='bg-slate-400 p-4 m-5 rounded-3xl'>
           <h1 className='font-extrabold text-xl mt-5'>id: {id}</h1>
           <h1 className='font-extrabold text-xl mt-5'>name: {name}</h1>
           <h1 className='font-extrabold text-xl mt-5'>price: {price}</h1>
-          <button onClick={()=>addToCart(id)} className='bg-blue-700 m-2 p-2 text-lg font-bold rounded-lg hover:bg-orange-700'>Add to card</button>
+          <button onClick={()=>addToCart(cosmetic)} className='bg-blue-700 m-2 p-2 text-lg font-bold rounded-lg hover:bg-orange-700'>Add to card</button>
+          <button onClick={()=>removeFromCart(id)} className='bg-blue-700 m-2 p-2 text-lg font-bold rounded-lg hover:bg-orange-600'>Remove from card</button>
         </div>
     );
 };
--- src/utilities/fakeDb.js
+++ src/utilities/fakeDb.js
@@ -1,5 +1,7 @@
 // use local storage to manage cart data
-const addToDb = id =>{
+const addToDb = cosmetic =>{
+    console.log(cosmetic)
+    const {id,price, name} = cosmetic;
     let shoppingCart ={};
     //   get the shopping cart
     const storedCart = localStorage.getItem("shopping-cart");
@@ -14,6 +16,7 @@ const addToDb = id =>{
     // add quantity
     const quantity = shoppingCart[id];
     if(quantity){
+    //    shoppingCart.name= name;
         const newQuantity = quantity+1;
         shoppingCart[id]=newQuantity
         //  localStorage.setItem(id,newQuantity)
@@ -22,6 +25,26 @@ const addToDb = id =>{
         // localStorage.setItem(id,1)
         shoppingCart[id] = 1; 
     }
+    shoppingCart.name= name;
     localStorage.setItem("shopping-cart",JSON.stringify(shoppingCart));
 }
-export {addToDb}
+
+const removeFromeDb = id => {
+    let shoppingCart ={};
+    
+          const storedCart = localStorage.getItem("shopping-cart");
+        shoppingCart = JSON.parse(storedCart);
+        const quantity = shoppingCart[id];
+        if(quantity>0){
+            let newQuantity = quantity -1;
+            shoppingCart[id] = newQuantity;
+            localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart))
+        }
+        else {
+             delete shoppingCart[id];
+             delete shoppingCart["name"];
+             localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+             alert("no item available for this product")
+        }
+}
+export {addToDb, removeFromeDb}
